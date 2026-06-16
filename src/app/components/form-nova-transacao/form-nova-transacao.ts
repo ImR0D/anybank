3784a3cca1dc28b2afcf9efac1dfd01fe6f98904
Rodrigo Ferreira <rodrigo.ferreira@pca.com.br>
2026-06-16 15:56:11 -0300

    feat: adicionado componente de formulário para os parâmetros inicias da transação e adicionado na pagina principal

---
+++ src/app/components/form-nova-transacao/form-nova-transacao.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-nova-transacao',
+  imports: [],
+  templateUrl: './form-nova-transacao.html',
+  styleUrl: './form-nova-transacao.css',
+})
+export class FormNovaTransacao {}
