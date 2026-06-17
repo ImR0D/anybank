3f70d6ed6c20a7a1b6b33a33ca6415c1a2bcc083
Rodrigo Ferreira <rodrigo.ferreira@pca.com.br>
2026-06-17 10:13:20 -0300

    feat: adicionado formulário orientado a template e alterado validações

--- src/app/components/form-nova-transacao/form-nova-transacao.ts
+++ src/app/components/form-nova-transacao/form-nova-transacao.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.html',
   styleUrl: './form-nova-transacao.css',
 })
-export class FormNovaTransacao {}
+export class FormNovaTransacao {
+  valorTransacao = '00,00';
+  transactionType = '';
+
+  realizarTransacao() {
+    console.log('Transação em andamento...');
+    console.log('Selected Transaction Type: ', this.transactionType);
+    console.log('Valor: ', this.valorTransacao);
+    this.valorTransacao = '';
+    this.transactionType = '';
+  }
+}
